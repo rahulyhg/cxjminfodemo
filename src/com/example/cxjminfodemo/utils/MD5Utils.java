58c59f1e614d8aa9e9b7f5873468990e6721a135
tengzhenjiu <tengzhenjiu@qq.com>
2016-10-10 17:37:13 +0800

    2016年10月10日17:37:11

---
+++ src/com/example/cxjminfodemo/utils/MD5Utils.java
@@ -0,0 +1,26 @@
+package com.example.cxjminfodemo.utils;
+
+import java.security.MessageDigest;
+
+public class MD5Utils {
+	public static String encode(String str){
+		try {
+			MessageDigest digest = MessageDigest.getInstance("md5");
+			byte[] result = digest.digest(str.getBytes());
+			StringBuffer buffer=new StringBuffer();
+			for(byte b:result){
+				int number=b&255;
+				String numberStr = Integer.toHexString(number);
+				if(numberStr.length()==1){
+					buffer.append("0");
+				}
+				buffer.append(numberStr);
+			}
+			return buffer.toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "";
+		}
+	}
+
+}
